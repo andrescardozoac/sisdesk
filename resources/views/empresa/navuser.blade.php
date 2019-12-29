220bfd244127e7b59a9cc827fc8621eba712cacb
Andres Cardozo Acosta G_IBA <luisandrescardozo@gmail.com>
2019-12-29 10:18:02 -0500

    Ruta Logo Sisdesk

--- resources/views/empresa/navuser.blade.php
+++ resources/views/empresa/navuser.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light">
   <a href ="{{route('empresa.index')}}"class="navbar-brand"><img id="icono" class="img-responsive" 
-    src="http://127.0.0.1/sisdesk.com/resources/images/logo.PNG" style="width: 150px;"></a>
+    src="http://127.0.0.1/sisdesk/resources/images/logo.PNG" style="width: 150px;"></a>
 
   <ul class="nav flex-column text-center">
   <li class="nav-item">
