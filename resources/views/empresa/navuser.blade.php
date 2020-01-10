49eaf04c1ad2db89faccdf7dcbf2b8f1ef376d91
Andres Cardozo Acosta G_IBA <luisandrescardozo@gmail.com>
2020-01-10 01:11:30 -0500

    prueba

--- resources/views/empresa/navuser.blade.php
+++ resources/views/empresa/navuser.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light">
   <a href ="{{route('empresa.index')}}"class="navbar-brand"><img id="icono" class="img-responsive" 
-    src="http://3.136.201.21/sisdesk/resources/images/logo.PNG" style="width: 150px;"></a>
-
+    src="http://127.0.0.1/sisdesk/resources/images/logo.PNG" style="width: 150px;"></a>
+    {{ asset('images/mac.png') }}
   <ul class="nav flex-column text-center">
   <li class="nav-item">
     <span class="nav-link active">Bienvenido   {{ Auth::user()->name }} </span>
